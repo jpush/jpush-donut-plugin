2cc86c6a7b269c7ae3044380696604b937475800
huangshuni <15889638723@163.com>
2025-06-16 10:56:10 +0800

    111111

---
+++ android/plugin/build/generated/source/buildConfig/debug/com/donut/wx324c7e239a81ad0f/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.donut.wx324c7e239a81ad0f;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.donut.wx324c7e239a81ad0f";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String PLUGIN_ID = "wx324c7e239a81ad0f";
+}

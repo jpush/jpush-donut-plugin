571bd5dd2a6dbc27a5419eb12469bb78370fd764
huangshuni <15889638723@163.com>
2025-06-26 16:20:59 +0800

    fix gitignore

---
+++ ios/Pods/Target Support Files/Pods-wx324c7e239a81ad0f/Pods-wx324c7e239a81ad0f-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wx324c7e239a81ad0f : NSObject
+@end
+@implementation PodsDummy_Pods_wx324c7e239a81ad0f
+@end
---
+++ ios/resources/scripts/toolSign.sh
@@ -0,0 +1,29 @@
+REQUEST_URL="127.0.0.1:19749"
+
+echo "pwd ": $(pwd)
+echo "project path: " ${PROJECT_DIR}
+
+echo "connecting with devtools..."
+
+response=$(curl -s -w "%{http_code}" -o response.txt $REQUEST_URL/miniappplugin/signApp?BUILT_PRODUCTS_DIR=${BUILT_PRODUCTS_DIR})
+status_code=${response: -3}
+response_body=$(cat response.txt)
+
+echo "status code: $status_code"
+echo "response: $response_body"
+
+rm response.txt
+
+# 非 200 的情况都不阻塞 XCode 构建
+if [[ $status_code != "200" ]]; then
+    echo "connecting with devtools fail"
+    echo "resign ios app fail"
+    exit 1
+else
+  if [[ $response_body == "success" ]]; then
+    echo "resign ios app done"
+  else
+    echo "resign ios app fail"
+    exit 1
+  fi
+fi
